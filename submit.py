c66248b8d0ce29f4eb48321a0f107f44594e180d
HYC <yunchaohe@gmail.com>
2016-01-31 23:50:54 +0800

    add submit converting

--- submit.py
+++ submit.py
@@ -7,9 +7,17 @@ ids, topics, texts = test
 
 ratings = []
 for score in submit_predict:
-    if score > 0.75:
+    if score > 0.875:
         s = 2
-    elif score
+    elif score > 0.625:
+        s = 1
+    elif score > 0.375:
+        s = 0
+    elif score > 0.125:
+        s = -1
+    else:
+        s = -2
+    ratings.append(s)
 
 for t in texts:
     print(t)
@@ -17,4 +25,4 @@ for t in texts:
 with open("./tmp/submit.csv", 'w', newline='') as f:
     w = csv.writer(f, delimiter='\t')
     for i in range(len(ids)):
-        w.writerow([ids[i], topics[i], submit_predict[i]])
+        w.writerow([ids[i], topics[i], ratings[i]])
